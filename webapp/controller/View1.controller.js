5b13534934340cb4521bf223545a5332a033535a
shakyl333 <shakyl.mansuri@gmail.com>
2019-07-02 05:39:52 +0000

    dropdown button compelted

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,8 +4,11 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("zdropdown.zdropdown.controller.View1", {
+		
 		onInit: function () {
-
+			//var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZSM_PO_SRV/", true);
+			//this.getView().byId("oSelect").setModel(oModel);
+			
 		}
 	});
 });
